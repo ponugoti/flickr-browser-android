6183bb79127382cd187440ec07ca67dd18ac2db0
Vishal Ponugoti <vponugot@purdue.edu>
2016-05-06 00:06:48 +0530

    Add missing constructor to Photo class

--- app/src/main/java/bamtastic/flickerbrowser/Photo.java
+++ app/src/main/java/bamtastic/flickerbrowser/Photo.java
@@ -9,6 +9,15 @@ public class Photo {
     private String mTags;
     private String mImage;
 
+    public Photo(String mTitle, String mAuthor, String mAuthorId, String mLink, String mTags, String mImage) {
+        this.mTitle = mTitle;
+        this.mAuthor = mAuthor;
+        this.mAuthorId = mAuthorId;
+        this.mLink = mLink;
+        this.mTags = mTags;
+        this.mImage = mImage;
+    }
+
     public String getmTitle() {
         return mTitle;
     }
